d18f28a82defd11114b725e2b23a3cf20d381881
Asmaalafe55 <asmaalafe55@gmail.com>
2022-09-27 14:36:00 +0300

    first commit

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+      <ProductCart
+          v-for="product in inventory"
+          :key="product.id"
+          class="card"
+          :product="product"
+          :addToCart="addToCart"
+        />
+    </div>
+
+  </main>
+</template>
+
+<script>
+import ProductCart from '@/components/ProductCart.vue'
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCart
+  }
+}
+</script>
